f65cf7766497dd05e2c374a43a7f8e6f42be860b
Mohammed Vaghjipurwala <mohammed.vaghjipurwala@gmail.com>
2016-08-26 20:35:17 +0530

    Update FindEvenList.py

--- FindEvenList.py
+++ FindEvenList.py
@@ -1,3 +1,10 @@
+######################################################
+#
+#Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. 
+#Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+#
+######################################################
+
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 #print ([x for x in [y ** 2 for y in range(1,11)] if x % 2 == 0])
