1342f0be72f678f698ff668d153cd481789d55c2
Mohammed Vaghjipurwala <mohammed.vaghjipurwala@gmail.com>
2016-08-31 13:03:58 +0530

    Create TicTacToe_part_2.py

---
+++ TicTacToe_part_2.py
@@ -0,0 +1,26 @@
+def checkGrid(grid):
+	# rows
+	for x in range(0,3):
+		row = set([grid[x][0],grid[x][1],grid[x][2]])
+		if len(row) == 1 and grid[x][0] != 0:
+			return grid[x][0]
+
+	# columns
+	for x in range(0,3):
+		column = set([grid[0][x],grid[1][x],grid[2][x]])
+		if len(column) == 1 and grid[0][x] != 0:
+			return grid[0][x]
+
+	# diagonals
+	diag1 = set([grid[0][0],grid[1][1],grid[2][2]])
+	diag2 = set([grid[0][2],grid[1][1],grid[2][0]])
+	if len(diag1) == 1 or len(diag2) == 1 and grid[1][1] != 0:
+		return grid[1][1]
+
+	return 0
+
+also_no_winner = [[0, 1, 0],
+	[2, 1, 0],
+	[2, 1, 0]]
+
+print(checkGrid(also_no_winner)," Wins!!!")
